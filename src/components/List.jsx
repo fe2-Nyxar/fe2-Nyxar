eea3ecf06a84c375d36468c89b86601040b3e5ed
fe2-Nyxar <Nyxar49@gmail.com>
2023-12-25 06:43:55 +0100

    version: 0.2 This update includes: new fruit are in, new Sorting system, better experience using the app description: this version come to represent custom sorting +new features

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,63 +1,93 @@
 import React, { useState, useEffect} from 'react';
 import styles from "./List.module.css";
+import axios from 'axios';
 
 function List() {
-  const initialFruits = [{ id: 1, name: "pineapple", calories: 37 },
-  { id:3, name: "Banana", calories: 105 },
-  { id: 4, name: "apple", calories: 95 },
-  { id: 2, name: "orange", calories: 45 },
-  { id: 5, name: "coconut", calories: 159 }];
+  const [initialFruits, setinitialFruits] = useState([]);
+  const [fruitInput,setFruitInput] = useState("all");
 
-  
+  useEffect(()=>{
+    axios.get(`https://www.fruityvice.com/api/fruit/${fruitInput}`)
+    .then(response =>  setinitialFruits(response.data)  )
+    .catch(error => console.log(error));
+  },[fruitInput])
+
   const [filteredFruits, setFilteredFruits] = useState(initialFruits);
   const [selectedFruit, setselectedFruit] = useState(null);
   const [listingMethod,setListingMethod] = useState(false);
 
-useEffect(()=>{
-  sortList();
-},[listingMethod])
-  const sortList = (method= "by-alphabet") => {
+  useEffect(()=>{
+  sortListBySelect();
+  // sortListByInput();
+},[])
+
+
+
+
+  const sortListBySelect = (method= "by-alphabet") => {
     let OutputFruitList;
     if(!listingMethod){
       switch (method) {
         case "by-calories":
-          OutputFruitList =[...initialFruits].sort((a, b) => a.calories - b.calories);
+          OutputFruitList = Object.values(initialFruits).sort((a, b) => a.nutritions.calories - b.nutritions.calories).reverse();
           break;
-        case 'by-id':
-          OutputFruitList = [...initialFruits].sort((a, b) => a.id - b.id);
-          break;
-        case 'by-alphabet':
-          OutputFruitList = [...initialFruits].sort((a, b) => a.name.localeCompare(b.name));
+          case 'by-id':
+            OutputFruitList = Object.values(initialFruits).sort((a, b) => a.id - b.id);
+            break;
+            case 'by-alphabet':
+              OutputFruitList = Object.values(initialFruits).sort((a, b) => a.name.localeCompare(b.name));
           break;
         default:
           break;
       }
-    }else{
-      if(method !== ""){
-        const calorieLimit = parseFloat(method);
-        OutputFruitList = [...initialFruits].filter((a) => a.calories >= calorieLimit);
-      }else OutputFruitList =[...initialFruits]
-    }
+    };
     setFilteredFruits(OutputFruitList);
   };
 
+  const [attributSLBI, SetAttributSLBI]= useState({sortType:"by-calories",value:10}); // SLBI is short for the function sortListByInput, and this hook is used as attribut holder
+  const sortListByInput=(method,value)=>{
+    let OutputFruitList;
+    if(listingMethod){
+      if(method === "by-calories" && value !== ""){
+          const calorieLimit = parseFloat(value);
+          OutputFruitList = Object.values(initialFruits).filter((a) => a.calories >= calorieLimit);
+        }else if(method === "by-alphabet" && value !==""){
+          OutputFruitList =  Object.values(initialFruits).filter((fruit)=> fruit.name.toLowerCase() === value.toLowerCase())
+        }
+        else OutputFruitList =[...initialFruits]
+    }
+    setFilteredFruits(OutputFruitList)
+  }
+
   return (
     <>
-    
-    {listingMethod && <div><input type='text' placeholder='Enter calorie limit' onChange={(e)=>sortList(e.target.value)}/><br/><br/></div>}
-      {!listingMethod && <span> <h3>order by:</h3>
-        <select onChange={(e) => sortList(e.target.value)}>
+    {listingMethod && (<div>  
+    <select onChange={(e) => SetAttributSLBI({...attributSLBI,sortType:e.target.value})}>
+        <option value={"by-calories"}>calorie</option>
+        <option value={"by-alphabet"}>alphabet</option>
+      </select>
+    <input type='text' placeholder='Enter your fruit' onChange={(e)=>SetAttributSLBI({...attributSLBI, value:e.target.value})}/><br/><br/>
+      <button onClick={()=>sortListByInput(attributSLBI.sortType, attributSLBI.value)}>Sort</button>
+    </div>)
+    }
+
+      {!listingMethod && (<span> <h3>order by:</h3>
+        <select onChange={(e) => sortListBySelect(e.target.value)}>
         <option value={"by-alphabet"}>alphabet</option>
         <option value={"by-id"}>Id</option>
         <option value={"by-calories"}>Calories</option>
       </select>
-      </span> 
+      </span>)
+      
       }
 
       <br/><ul>
-      {filteredFruits.map(fruit => (
-        <li className={styles.Fruitsbracket} key={fruit.id} onClick={()=>setselectedFruit(fruit)}>
-          {fruit.name}</li>))}
+      {filteredFruits && filteredFruits.map((fruit, index) => (
+  <li className={styles.Fruitsbracket} key={fruit.id} onClick={() => setselectedFruit(fruit)}>
+    {fruit.name}
+  </li>
+))}
+
         <br />
       </ul>      
       
@@ -65,7 +95,7 @@ useEffect(()=>{
       {selectedFruit && !listingMethod && (
         <div>
           <p>selected Fruit: {selectedFruit.name}</p>
-          <p>Calories: {selectedFruit.calories}</p>
+          <p>Calories: {selectedFruit.nutritions.calories}</p>
         </div>
       )}
       <button onClick={(e)=>{setListingMethod((prevValue)=>!prevValue)}}>switch listing Method</button>
