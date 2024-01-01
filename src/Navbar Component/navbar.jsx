e19ec6589a0ccf33a4836ecb96cc77560c9312de
fe2-Nyxar <Nyxar49@gmail.com>
2024-01-01 02:35:24 +0100

    just changing names of folders, starting a new style 'navbar'

---
+++ src/Navbar Component/navbar.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import "./NavBar.modules.css";
+function Navbar() {
+  const [themeMode, setThemeMode] = useState(true);
+
+  return <div id="navbar">navbar</div>;
+}
+
+export default Navbar;
