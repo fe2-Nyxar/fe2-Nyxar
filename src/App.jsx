1b39f57867c7786df2996de583e981de9e791aaa
fe2-Nyxar <Nyxar49@gmail.com>
2024-01-01 02:33:14 +0100

    just changing names around

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-import './App.css'
-import List from './components/List';
+import "./App.css";
+import List from "./List Component/List";
+import Navbar from "./Navbar Component/navbar";
 
 function App() {
-
-
-
-  return(
+  return (
     <>
-      <List/>
+      <Navbar />
+      <List />
     </>
-  )
+  );
 }
-export default App
+export default App;
